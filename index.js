5e52db7e4a122f5a8d69665a6c298677399e1486
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-04 13:03:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+
+const bot = new Discord.Client();
+const config = require("./config.json");
+bot.config = config;
+bot.logsEmbed = new Discord.RichEmbed()
+    .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
+        .toString(16)
+        .substr(1, 6))
+    .setTimestamp()
+    .setDescription("")
+    .setTitle("");
+    logsEmbed.fields=[];
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    bot.on(eventName, event.bind(null,  bot));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    bot.commands.set(commandName, props);
+  });
+});
+
+bot.login(config.token);
