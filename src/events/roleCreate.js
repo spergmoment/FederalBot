732590f3fef7131a20abfefa440a02e1c71a6861
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 08:11:32 -0500

    Update roleCreate.js

--- src/events/roleCreate.js
+++ src/events/roleCreate.js
@@ -2,13 +2,15 @@ module.exports = (bot, r) => {
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
     .setDescription("")
-    .setTitle("")
+    .setTitle("");
     const logs = r.guild.channels.find(r => r.name === ("logs"));
+    const entry = ch.guild.fetchAuditLogs({type: 30}).then(audit => audit.entries.first());
     if (logs) {
         logsEmbed.setTitle("Action: Create Role")
             .addField("Name", r.name)
             .addField("Position", r.calculatedPosition)
             .addField("Hex", r.hexColor)
+            .addField("Perpetrator", entry.executor.username)
             .addField("IDs", "```Role ID: " + r.id + "```");
         logs.send(logsEmbed);
     }
