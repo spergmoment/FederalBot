0cd5dc12b840b464c4a8c7f874ebd4bc40e58bb7
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 13:05:31 -0500

    formatting, await

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -1,7 +1,7 @@
 exports.run = (msg, bot, args) => {
-            if (msg.member.roles.find(r => r.name === "Bot Owner")) {
-                        msg.delete.then(() => {
-            process.exit();
-                        });
-        }
+    if (msg.member.roles.find(r => r.name === "Bot Owner")) {
+        msg.delete.then(async m => {
+            await process.exit();
+        });
+    }
 };
