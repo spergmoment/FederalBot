21312118e262f75a1d860595bb5c401746b82d47
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 10:46:11 -0500

    Update reset.js

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -1,5 +1,7 @@
 exports.run = (msg, bot, args) => {
             if (msg.member.roles.find(r => r.name === "Bot Owner")) {
+                        msg.delete.then(() => {
             process.exit();
+                        });
         }
 };
