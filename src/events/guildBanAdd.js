f3e6bac882dfaf5e703e3aa76ebfbcde4c4483ac
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 08:09:42 -0500

    a

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -3,10 +3,12 @@ module.exports = (bot, g, m) => {
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
     .setDescription("")
-    .setTitle("")
+    .setTitle("");
+    const entry = g.fetchAuditLogs({type: 23}).then(audit => audit.entries.first());
     if (logs) {
         logsEmbed.setTitle("Action: Ban")
             .addField("Banned User", m.username)
+            .addField("Perpetrator", entry.executor.username)
             .addField("IDs", "```User ID: " + m.id + "```");
         logs.send(logsEmbed);
     }
