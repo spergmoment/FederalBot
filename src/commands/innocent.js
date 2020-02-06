96526a63f71c0f9db17baafd53bb06d92a3b8b50
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 14:23:30 -0500

    add aliases

--- src/commands/innocent.js
+++ src/commands/innocent.js
@@ -1,97 +1,55 @@
-exports.run = (msg, bot, args) => {
-    if (bot.judgeToUse && bot.detainer && bot.courtThing) {
-        const Discord = require("discord.js");
-        const inno = new Discord.RichEmbed()
-            .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-            .setTimestamp()
-            .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
-                .toString(16)
-                .substr(1, 6));
-        if (msg.channel.parent === "court") {
-            if (msg.member.user.id === bot.judgeToUse.user.id) {
-                if (args.length > 0) {
-                    inno.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **INNOCENT.**");
-                    inno.setFooter('Ruled this case as innocent.');
-                    msg.channel.send(inno);
-                    var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
-                    var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
-                    msg.channel.replacePermissionOverwrites({
-                        overwrites: [{
-                            id: cj.id,
-                            deny: ['SEND_MESSAGES'],
+module.exports = {
+    name: 'innocent',
+    description: 'marks someone in a court case as innocent',
+    aliases: ['inno', 'not_guilty', 'not-guilty'],
+    execute(msg, bot, args) {
+        if (bot.judgeToUse && bot.detainer && bot.courtThing) {
+            const Discord = require("discord.js");
+            const inno = new Discord.RichEmbed()
+                .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
+                .setTimestamp()
+                .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
+                    .toString(16)
+                    .substr(1, 6));
+            if (msg.channel.parent === "court") {
+                if (msg.member.user.id === bot.judgeToUse.user.id) {
+                    if (args.length > 0) {
+                        inno.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **INNOCENT.**");
+                        inno.setFooter('Ruled this case as innocent.');
+                        msg.channel.send(inno);
+                        var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
+                        var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
+                        msg.channel.replacePermissionOverwrites({
+                            overwrites: [{
+                                id: cj.id,
+                                deny: ['SEND_MESSAGES'],
                         }, {
-                            id: cp.id,
-                            deny: ['SEND_MESSAGES'],
+                                id: cp.id,
+                                deny: ['SEND_MESSAGES'],
                         }, {
-                            id: bot.detainer.id,
-                            deny: ['SEND_MESSAGES'],
+                                id: bot.detainer.id,
+                                deny: ['SEND_MESSAGES'],
                         }, {
-                            id: bot.judgeToUse.user.id,
-                            deny: ['SEND_MESSAGES'],
+                                id: bot.judgeToUse.user.id,
+                                deny: ['SEND_MESSAGES'],
                         }, {
-                            id: bot.courtThing.user.id,
-                            deny: ['SEND_MESSAGES'],
+                                id: bot.courtThing.user.id,
+                                deny: ['SEND_MESSAGES'],
                         }, ],
-                    });
-                    bot.logEmbed.setTitle("Action: Rule case as Innocent");
-                    bot.logEmbed.setDescription("Perpetrator: " + msg.member.displayName);
-                    bot.logs.send(bot.logEmbed);
-                } else if (args.length === 0) {
-                    inno.setDescription(msg.member.displayName + ", please provide a reason.");
-                    inno.setFooter('Reason unspecified.');
+                        });
+                        bot.logEmbed.setTitle("Action: Rule case as Innocent");
+                        bot.logEmbed.setDescription("Perpetrator: " + msg.member.displayName);
+                        bot.logs.send(bot.logEmbed);
+                    } else if (args.length === 0) {
+                        inno.setDescription(msg.member.displayName + ", please provide a reason.");
+                        inno.setFooter('Reason unspecified.');
+                    }
                 }
+            } else {
+                inno.setDescription(msg.member.displayName + ", this command may only be used in a court case.");
+                inno.setFooter('Category ' + msg.channel.parent + ' does not match category "court".');
             }
-        } else {
-            inno.setDescription(msg.member.displayName + ", this command may only be used in a court case.");
-            inno.setFooter('Category ' + msg.channel.parent + ' does not match category "court".');
+            msg.channel.send(inno);
         }
-        msg.channel.send(inno);
-    }
-};exports.run = (msg, bot, args) => {
-    const Discord = require("discord.js");
-    const inno = new Discord.RichEmbed()
-            .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-            .setTimestamp()
-            .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
-                .toString(16)
-                .substr(1, 6));
-        if (msg.channel.parent === "court") {
-            if (msg.member.user.id === bot.judgeToUse.user.id) {
-                if (args.length > 0) {
-                    inno.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **INNOCENT.**");
-                    inno.setFooter('Ruled this case as innocent.');
-                    msg.channel.send(inno);
-                                    var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
-                var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
-                    msg.channel.replacePermissionOverwrites({
-                        overwrites: [{
-                            id: cj.id,
-                            deny: ['SEND_MESSAGES'],
-                        }, {
-                            id: cp.id,
-                            deny: ['SEND_MESSAGES'],
-                        }, {
-                            id: bot.detainer.id,
-                            deny: ['SEND_MESSAGES'],
-                        }, {
-                            id: bot.judgeToUse.user.id,
-                            deny: ['SEND_MESSAGES'],
-                        }, {
-                            id: bot.courtThing.user.id,
-                            deny: ['SEND_MESSAGES'],
-                        }, ],
-                    });
-                    bot.logEmbed.setTitle("Action: Rule case as Innocent");
-                    bot.logEmbed.setDescription("Perpetrator: " + msg.member.displayName);
-                    bot.logs.send(bot.logEMbed);
-                } else if (args.length === 0) {
-                    inno.setDescription(msg.member.displayName + ", please provide a reason.");
-                    inno.setFooter('Reason unspecified.');
-                }
-            }
-        } else {
-            inno.setDescription(msg.member.displayName + ", this command may only be used in a court case.");
-            inno.setFooter('Category ' + msg.channel.parent + ' does not match category "court".');
-        }
-        msg.channel.send(inno);
+    },
 };
