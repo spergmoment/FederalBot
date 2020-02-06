781713cf0473ad53794a7cfaf822f7a59f3c0262
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 08:10:19 -0500

    Update guildBanRemove.js

--- src/events/guildBanRemove.js
+++ src/events/guildBanRemove.js
@@ -3,10 +3,12 @@ module.exports = (bot, g, m) => {
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
     .setDescription("")
-    .setTitle("")
+    .setTitle("");
+    const entry = ch.guild.fetchAuditLogs({type: 23}).then(audit => audit.entries.first());
     if (logs) {
         logsEmbed.setTitle("Action: Unban")
             .addField("Unbanned User", m.username)
+            .addField("Perpetrator", entry.executor.username)
             .addField("IDs", "```User ID: " + m.id + "```");
         logs.send(logsEmbed);
     }
