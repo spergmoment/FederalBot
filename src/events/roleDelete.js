aee2636af0bcc0532e2b516893683a17d3317ed5
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-06 08:12:10 -0500

    Update roleDelete.js

--- src/events/roleDelete.js
+++ src/events/roleDelete.js
@@ -2,13 +2,15 @@ module.exports = (bot, r) => {
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
     .setDescription("")
-    .setTitle("")
+    .setTitle("");
     const logs = r.guild.channels.find(r => r.name === ("logs"));
+    const entry = r.guild.fetchAuditLogs({type: 32}).then(audit => audit.entries.first());
     if (logs) {
         logsEmbed.setTitle("Action: Delete Role")
             .addField("Name", r.name)
             .addField("Position", r.calculatedPosition)
             .addField("Hex", r.hexColor)
+            .addField("Perpetrator", entry.executor.username)
             .addField("IDs", "```Role ID: " + r.id + "```");
         logs.send(logsEmbed);
     }
