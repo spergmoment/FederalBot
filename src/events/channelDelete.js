1514727b8062bcfe7a708e80ee6c07907bdcdfdc
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-09 00:15:09 -0500

    was

--- src/events/channelDelete.js
+++ src/events/channelDelete.js
@@ -2,14 +2,16 @@ module.exports = (bot, ch) => {
     const logs = ch.guild.channels.find(r => r.name === ("logs"));
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
-    .setDescription("")
-    .setTitle("");
-    const entry = ch.guild.fetchAuditLogs({type: 12}).then(audit => audit.entries.first());
-    if (logs) {
-        logsEmbed.setTitle("Action: Delete Channel")
-            .addField("Channel Name", ch.name)
-            .addField("Perpetrator", entry.executor.username)
-            .addField("IDs", "```Channel ID: " + ch.id + "```");
-        logs.send(logsEmbed);
-    }
-}
+        .setDescription("")
+        .setTitle("");
+    const entry = ch.guild.fetchAuditLogs({ type: 12 }).then(audit => {
+        const e = audit.entries.first();
+        if (logs) {
+            logsEmbed.setTitle("Action: Delete Channel")
+                .addField("Channel Name", ch.name)
+                .addField("Perpetrator", e.executor.username)
+                .addField("IDs", "```Channel ID: " + ch.id + "```");
+            logs.send(logsEmbed);
+        }
+    });
+};
