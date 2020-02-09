9ee5ac7a33d2a17b2190d67781bd9062fa7cdc21
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-09 00:16:51 -0500

    was

--- src/events/guildBanRemove.js
+++ src/events/guildBanRemove.js
@@ -2,14 +2,16 @@ module.exports = (bot, g, m) => {
     const logs = g.channels.find(r => r.name === ("logs"));
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
-    .setDescription("")
-    .setTitle("");
-    const entry = ch.guild.fetchAuditLogs({type: 23}).then(audit => audit.entries.first());
-    if (logs) {
-        logsEmbed.setTitle("Action: Unban")
-            .addField("Unbanned User", m.username)
-            .addField("Perpetrator", entry.executor.username)
-            .addField("IDs", "```User ID: " + m.id + "```");
-        logs.send(logsEmbed);
-    }
-}
+        .setDescription("")
+        .setTitle("");
+    const entry = g.fetchAuditLogs({ type: 23 }).then(audit => {
+        const e = audit.entries.first();
+        if (logs) {
+            logsEmbed.setTitle("Action: Unban")
+                .addField("Unbanned User", m.username)
+                .addField("Perpetrator", e.executor.username)
+                .addField("IDs", "```User ID: " + m.id + "```");
+            logs.send(logsEmbed);
+        }
+    });
+};
