9d05d6a7bf94982feb2fac95f93c64ba70fd4131
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-09 00:17:24 -0500

    wasd

--- src/events/roleCreate.js
+++ src/events/roleCreate.js
@@ -1,17 +1,19 @@
 module.exports = (bot, r) => {
     const Discord = require("discord.js");
     const logsEmbed = new Discord.RichEmbed()
-    .setDescription("")
-    .setTitle("");
+        .setDescription("")
+        .setTitle("");
     const logs = r.guild.channels.find(r => r.name === ("logs"));
-    const entry = r.guild.fetchAuditLogs({type: 30}).then(audit => audit.entries.first());
-    if (logs) {
-        logsEmbed.setTitle("Action: Create Role")
-            .addField("Name", r.name)
-            .addField("Position", r.calculatedPosition)
-            .addField("Hex", r.hexColor)
-            .addField("Perpetrator", entry.executor.username)
-            .addField("IDs", "```Role ID: " + r.id + "```");
-        logs.send(logsEmbed);
-    }
-}
+    const entry = r.guild.fetchAuditLogs({ type: 30 }).then(audit => {
+        const e = audit.entries.first();
+        if (logs) {
+            logsEmbed.setTitle("Action: Create Role")
+                .addField("Name", r.name)
+                .addField("Position", r.calculatedPosition)
+                .addField("Hex", r.hexColor)
+                .addField("Perpetrator", entry.executor.username)
+                .addField("IDs", "```Role ID: " + r.id + "```");
+            logs.send(logsEmbed);
+        }
+    });
+};
