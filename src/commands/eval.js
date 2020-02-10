553efa7d51d8b2521e7fb37bb5a9fbb35d3ae420
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-10 12:36:06 -0500

    dont del the command, implement async into exports.run

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,32 +1,29 @@
-exports.run = (msg, bot, args) => {
+exports.run = async (msg, bot, args) => {
     const util = require("util");
     const Discord = require("discord.js");
-    msg.delete()
-        .then(async m => {
-            if (msg.member.roles.find(r => r.name === "Bot Owner")) {
-                try {
-                    let result = eval(args.join(" "));
-                    if (result instanceof Promise) {
-                        result = await result;
-                    }
-                    if (typeof result !== 'string') {
-                        result = util.inspect(result, {
-                            depth: 0
-                        });
-                    }
-                    result = result.replace(msg.client.token, '[TOKEN HIDDEN]');
-                    const evalEmbed = new Discord.RichEmbed()
-                        .setColor("RANDOM")
-                        .addField('Eval', '```js\n' + args.join(" ") + '```')
-                        .addField('Returns', '```js\n' + result + '```');
-                    msg.channel.send(evalEmbed);
-                } catch (err) {
-                    const evalEmbed = new Discord.RichEmbed()
-                        .setColor("RED")
-                        .addField('Eval', '```js\n' + args.join(" ") + '```')
-                        .addField('Error', '```js\n' + err + '```');
-                    msg.channel.send(evalEmbed);
-                }
+    if (msg.member.roles.find(r => r.name === "Bot Owner")) {
+        try {
+            let result = eval(args.join(" "));
+            if (result instanceof Promise) {
+                result = await result;
             }
-        });
+            if (typeof result !== 'string') {
+                result = util.inspect(result, {
+                    depth: 0
+                });
+            }
+            result = result.replace(msg.client.token, '[TOKEN HIDDEN]');
+            const evalEmbed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .addField('Eval', '```js\n' + args.join(" ") + '```')
+                .addField('Returns', '```js\n' + result + '```');
+            msg.channel.send(evalEmbed);
+        } catch (err) {
+            const evalEmbed = new Discord.RichEmbed()
+                .setColor("RED")
+                .addField('Eval', '```js\n' + args.join(" ") + '```')
+                .addField('Error', '```js\n' + err + '```');
+            msg.channel.send(evalEmbed);
+        }
+    }
 };
