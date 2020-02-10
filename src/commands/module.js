aa7e4e6ac1b315a443eae0d7501a591ee2c3a316
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-10 01:06:04 -0500

    sex

--- src/commands/module.js
+++ src/commands/module.js
@@ -1,6 +1,6 @@
 exports.run = (msg, bot, args) => {
     const Discord = require("discord.js");
-    let d = args[0].toLowerCase();
+    if(args[0]) let d = args[0].toLowerCase();
     const module = new Discord.RichEmbed()
         .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
         .setTimestamp()
