49a8c5292055a0f724358ad9e146bbf10bd1ecf7
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-10 22:03:14 -0500

    aaa

--- src/commands/detain.js
+++ src/commands/detain.js
@@ -7,26 +7,6 @@ exports.run = (msg, bot, args) => {
         .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
             .toString(16)
             .substr(1, 6));
-    bot.detainer = msg.author;
-    let role = msg.guild.roles.find(r => r.name === "Detained");
-    if (args.length < 1) {
-        det.setDescription("Please specify which user to detain.");
-        det.setFooter('Argument 0 is undefined');
-    } else if (args.length < 2) {
-        det.setDescription("Please specify the reason.");
-        det.setFooter('Argument 1 is undefined');
-    } else {
-        if (!msg.member.roles.find(r => r.name === "Officer") && !msg.member.roles.find(r => r.name === "Chief of Police")) {
-            det.setDescription("You must be an officer to use this command.");
-            det.setFooter('You lack texports.run = (msg, bot, args) => {
-    const Discord = require("discord.js");
-    let member = msg.mentions.members.first();
-    const det = new Discord.RichEmbed()
-        .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-        .setTimestamp()
-        .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
-            .toString(16)
-            .substr(1, 6));
     bot.detainer = msg.member;
     let role = msg.guild.roles.find(r => r.name === "Detained");
     if (args.length < 1) {
@@ -40,9 +20,9 @@ exports.run = (msg, bot, args) => {
             det.setDescription("You must be an officer to use this command.");
             det.setFooter('You lack the Officer role in the Permission Object.');
         } else {
-            det.setDescription(msg.member.displayName + ", I have detained " + member.displayName + 
-            ", for reason " + args[1] + ". A judge must **;approve** this detain within **5 minutes** or you will be IMPEACHED!"); 
-          /*the above was split in 2 to take up less space*/
+            det.setDescription(msg.member.displayName + ", I have detained " + member.displayName +
+                ", for reason " + args[1] + ". A judge must **;approve** this detain within **5 minutes** or you will be IMPEACHED!");
+            /*the above was split in 2 to take up less space*/
             bot.reason = (args[1]);
             det.setFooter('User ' + member.displayName + " has been detained.");
             bot.logEmbed.setTitle("Action: Detain");
