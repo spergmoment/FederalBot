c966d82e873a4b5c071bda778ac88bd5d873886a
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-12 14:22:58 -0500

    Update impeach.js

--- src/commands/impeach.js
+++ src/commands/impeach.js
@@ -5,9 +5,7 @@ exports.run = (msg, bot, args) => {
         const peach = new Discord.RichEmbed()
             .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
             .setTimestamp()
-            .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
-                .toString(16)
-                .substr(1, 6));
+            .setColor('RANDOM');
         if (args) {
             let role = undefined;
             if (msg.member.roles.find(r => r.name === "Speaker of the House")) {
