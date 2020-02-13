9f3d01d7bc840b6e0658ab14ef60c494ac4d9c2e
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-13 12:31:47 -0500

    updated a few things

--- src/commands/guilty.js
+++ src/commands/guilty.js
@@ -1,7 +1,7 @@
 exports.run = (msg, bot, args) => {
     if (bot.judgeToUse && bot.detainer && bot.courtThing) {
         const Discord = require("discord.js");
-        let free = function () {
+        let free = function() {
             bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Muted"));
         };
         const guilty = new Discord.RichEmbed()
@@ -10,9 +10,10 @@ exports.run = (msg, bot, args) => {
             .setColor('RANDOM');
         if (msg.channel.parent.name === "court") {
             if (msg.member.user.id === bot.judgeToUse.user.id) { // makes sure it's used in court by the selected judge
-                if (args.length > 1) {
+                if (args.length > 1 && bot.reason !== 1 && bot.reason !== 9) {
                     bot.courtThing.addRole(msg.guild.roles.find(r => r.name === "Muted"));
-                    guilty.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **GUILTY.** If a time other than 0 has been put on a crime not punishable by imprisonment, you will be IMPEACHED!"); // you will
+                    bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Court"));
+                    guilty.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **GUILTY.**");
                     guilty.setFooter('Ruled the case as guilty.');
                     msg.channel.send(guilty);
                     var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
@@ -36,18 +37,28 @@ exports.run = (msg, bot, args) => {
                         }, {
                             id: msg.guild.defaultRole.id,
                             deny: ["SEND_MESSAGES"],
-                        },],
+                        }, ],
                     });
-                    setTimeout(free, (args[0] * 3600000));
-                    bot.logEmbed.setTitle("Action: Rule case as Guilty");
-                    bot.logEmbed.setDescription("Perpetrator: " + msg.member.displayName);
+                    if (bot.reason === 1 || bot.reason === 9) {
+                        if (bot.courtThing.misdemeanors < 4) {
+                            setTimeout(free, 0);
+                            bot.courtThing.misdemeanors += 1;
+                        }
+                    } else {
+                        setTimeout(free, (args[0] * 3600000));
+                    }
+                    bot.logEmbed.setTitle("Rule case as Guilty");
+                    bot.logEmbed.addField("Perpetrator", msg.member.displayName)
+                        .addField("Reason", args.slice(0, args.length - 1));
                     bot.logs.send(bot.logEmbed);
                 } else if (args.length === 0) {
                     guilty.setDescription(msg.member.displayName + ", please provide a reason.");
                     guilty.setFooter('No reason found.');
                 } else {
-                    guilty.setDescription(msg.member.displayName + ", please set a time of imprisonment. If this crime is for a 1st-3rd misdemeanor, please put `0`.");
-                    guilty.setFooter('No imprisonment time specified.');
+                    if (bot.reason !== 1 && bot.reason !== 9 && bot.courtThing.misdemeanors < 4) {
+                        guilty.setDescription(msg.member.displayName + ", please set a time of imprisonment.");
+                        guilty.setFooter('No imprisonment time specified.');
+                    }
                 }
             }
         } else {
