45809506703ed86a4ddda22f171b0d135e10e31b
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-19 12:32:15 -0500

    efficiency, useless vars, color

--- src/events/message.js
+++ src/events/message.js
@@ -8,14 +8,11 @@ module.exports = (bot, msg) => {
     const command = args.shift()
         .toLowerCase();
     const cmd = bot.commands.get(command);
-    var guild = msg.guild; // shortens code
-    bot.logs = guild.channels.find(ch => ch.name === 'logs' && ch.type == "text"); // logs channel
+    bot.logs = msg.guild.channels.find(ch => ch.name === 'logs' && ch.type == "text"); // logs channel
     bot.logEmbed = new Discord.RichEmbed()
         .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
         .setTimestamp()
-        .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
-            .toString(16)
-            .substr(1, 6)); // the logs embed lol
+        .setColor('RANDOM'); // the logs embed lol
     if (!cmd) return;
     cmd.run(msg, bot, args);
 };
