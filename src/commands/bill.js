de8f16dd8f2c5a642d35def9c10948b810d55700
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-19 12:58:38 -0500

    Update bill.js

--- src/commands/bill.js
+++ src/commands/bill.js
@@ -1,36 +1,41 @@
-  exports.run = (msg, bot, args) => {
+exports.run = (msg, bot, args) => {
     const Discord = require("discord.js");
     const bill = new Discord.RichEmbed()
-            .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-            .setTimestamp()
-            .setColor('RANDOM');
-        if (msg.member.roles.find(r => r.name === "Congress")||msg.member.roles.ind(r => r.name==="Bot Owner")) {
-            if (args.length === 0) {
-                bill.setDescription("Please enter content for the bill.");
-                bill.setFooter("No message has been found.");
-                msg.channel.send(bill);
-            } else {
-                const billMsg = args.join(" ");
-                bill.setTitle("Bill in Congress by " + msg.member.displayName + ".");
-                bill.setDescription(billMsg);
-                bill.setFooter('Bill proposed!');
-                msg.delete()
-                    .catch(O_o => {});
-                msg.channel.send(bill)
-                    .then(async msg => {
-                        try {
-                            await msg.pin();
-                            await msg.react(msg.guild.emojis.find(emoji => emoji.name === "Upvote"));
-                            await msg.react(msg.guild.emojis.find(emoji => emoji.name === "Downvote"));
-                            await msg.channel.send("<&" + msg.guild.roles.find(r => r.name==="Congress").id + "> bill in <#" + msg.guild.channels.find(c => c.name==="congress"&&c.type==="text").id + ">"); // @Congress bill in #congress
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    });
-            }
-        } else {
-            bill.setDescription("You must be in Congress to use this command");
-            bill.setFooter("Missing role: \"Congress\"");
+        .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
+        .setTimestamp()
+        .setColor('RANDOM');
+    if (msg.member.roles.find(r => r.name === "Congress") || msg.member.roles.ind(r => r.name === "Bot Owner")) {
+        if (args.length === 0) {
+            bill.setDescription("Please enter content for the bill.");
+            bill.setFooter("No message has been found.");
             msg.channel.send(bill);
+        } else {
+            msg.channel.send("Creating bill...")
+                .then(async m => {
+                    const billMsg = args.join(" ");
+                    bill.setTitle("Bill in Congress by " + msg.member.displayName + ".");
+                    bill.setDescription(billMsg);
+                    bill.setFooter('Bill proposed!');
+                    msg.delete()
+                        .catch(O_o => {});
+                    msg.channel.send(bill)
+                        .then(async msg => {
+                            try {
+                                await msg.pin();
+                                await msg.react(msg.guild.emojis.find(emoji => emoji.name === "Upvote"));
+                                await msg.react(msg.guild.emojis.find(emoji => emoji.name === "Downvote"));
+                                await msg.channel.send("<&" + msg.guild.roles.find(r => r.name === "Congress")
+                                    .id + "> bill in <#" + msg.guild.channels.find(c => c.name === "congress" && c.type === "text")
+                                    .id + ">"); // @Congress bill in #congress
+                            } catch (error) {
+                                console.log(error);
+                            }
+                        });
+                });
         }
+    } else {
+        bill.setDescription("You must be in Congress to use this command");
+        bill.setFooter("Missing role: \"Congress\"");
+        msg.channel.send(bill);
+    }
 };
