27a47f86ae0c7df440d7197f3a660d684748650b
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-19 13:10:30 -0500

    check if law is valid

--- src/commands/law.js
+++ src/commands/law.js
@@ -1,6 +1,9 @@
 exports.run = (msg, bot, args) => {
-    const Discord = require("discord.js");
-    var laws = [
+    if (args[0] > 9 || args[0] < 1) return msg.channel.send("That is not a valid law!");
+    msg.channel.send("Fetching law " + args[0] + "...")
+        .then(m => {
+            const Discord = require("discord.js");
+            var laws = [
         "**Law 1: Spam**\nSending an unreasonably large amount of messages in a short amount of time. (Misdemeanor)",
         "**Law 2: Raid**\nSending a massive amount of messages in a very short time, or otherwise participating in a large attack on our server, is prohibited. (Banishment)",
         "**Law 3: Alt**\nUsing more than a single alternative account, or otherwise using an alt to your advantage, is prohibited. (Felony)",
@@ -11,12 +14,14 @@ exports.run = (msg, bot, args) => {
         "**Law 8: Obstruction**\nDestroying evidence in any such way that it can be used for your own advantage is strictly prohibited. (Automatic loss of case, Impeachment (Both I/A))",
         "**Law 9: Invite**\nPurposefully sending any valid invite link to any server other than our own is prohibited. (Misdemeanor)"
     ];
-    var lawThing = (laws[args[0] - 1]);
-    const lawSend = new Discord.RichEmbed()
-        .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-        .setTimestamp()
-        .setColor('RANDOM');
-    lawSend.setDescription(lawThing);
-    lawSend.setFooter('Law ' + args[0]);
-    msg.channel.send(lawSend);
+            var lawThing = (laws[args[0] - 1]);
+            const lawSend = new Discord.RichEmbed()
+                .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
+                .setTimestamp()
+                .setColor('RANDOM');
+            lawSend.setDescription(lawThing);
+            lawSend.setFooter('Law ' + args[0]);
+            msg.channel.send(lawSend);
+            m.delete();
+        });
 };
