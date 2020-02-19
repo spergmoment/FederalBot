980ebc10c4939ac27ee268d8f6eccc7d2b49d3e6
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-19 13:02:12 -0500

    Update guilty.js

--- src/commands/guilty.js
+++ src/commands/guilty.js
@@ -11,47 +11,51 @@ exports.run = (msg, bot, args) => {
         if (msg.channel.parent.name === "court") {
             if (msg.member.user.id === bot.judgeToUse.user.id) { // makes sure it's used in court by the selected judge
                 if (args.length > 1 && bot.reason !== 1 && bot.reason !== 9) {
-                    bot.courtThing.addRole(msg.guild.roles.find(r => r.name === "Muted"));
-                    bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Court"));
-                    guilty.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **GUILTY.**");
-                    guilty.setFooter('Ruled the case as guilty.');
-                    msg.channel.send(guilty);
-                    var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
-                    var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
-                    msg.channel.replacePermissionOverwrites({
-                        overwrites: [{
-                            id: cj.id,
-                            deny: ['SEND_MESSAGES'],
+                    msg.channel.send("Ruling case as guilty...")
+                        .then(m => {
+                            bot.courtThing.addRole(msg.guild.roles.find(r => r.name === "Muted"));
+                            bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Court"));
+                            guilty.setDescription(msg.member.displayName + ", " + bot.courtThing.displayName + " has been found **GUILTY.**");
+                            guilty.setFooter('Ruled the case as guilty.');
+                            msg.channel.send(guilty);
+                            var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
+                            var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
+                            msg.channel.replacePermissionOverwrites({
+                                overwrites: [{
+                                    id: cj.id,
+                                    deny: ['SEND_MESSAGES'],
                         }, {
-                            id: cp.id,
-                            deny: ['SEND_MESSAGES'],
+                                    id: cp.id,
+                                    deny: ['SEND_MESSAGES'],
                         }, {
-                            id: bot.detainer.id,
-                            deny: ['SEND_MESSAGES'],
+                                    id: bot.detainer.id,
+                                    deny: ['SEND_MESSAGES'],
                         }, {
-                            id: bot.judgeToUse.user.id,
-                            deny: ['SEND_MESSAGES'],
+                                    id: bot.judgeToUse.user.id,
+                                    deny: ['SEND_MESSAGES'],
                         }, {
-                            id: bot.courtThing.user.id,
-                            deny: ['SEND_MESSAGES'],
+                                    id: bot.courtThing.user.id,
+                                    deny: ['SEND_MESSAGES'],
                         }, {
-                            id: msg.guild.defaultRole.id,
-                            deny: ["SEND_MESSAGES"],
+                                    id: msg.guild.defaultRole.id,
+                                    deny: ["SEND_MESSAGES"],
                         }, ],
-                    });
-                    if (bot.reason === 1 || bot.reason === 9) {
-                        if (bot.courtThing.misdemeanors < 4) {
-                            setTimeout(free, 0);
-                            bot.courtThing.misdemeanors += 1;
-                        }
-                    } else {
-                        setTimeout(free, (args[0] * 3600000));
-                    }
-                    bot.logEmbed.setTitle("Rule case as Guilty");
-                    bot.logEmbed.addField("Perpetrator", msg.member.displayName)
-                        .addField("Reason", args.slice(0, args.length - 1));
-                    bot.logs.send(bot.logEmbed);
-                    bot.courtThing = "";
+                            });
+                            if (bot.reason === 1 || bot.reason === 9) {
+                                if (bot.courtThing.misdemeanors < 4) {
+                                    setTimeout(free, 0);
+                                    bot.courtThing.misdemeanors += 1;
+                                }
+                            } else {
+                                setTimeout(free, (args[0] * 3600000));
+                            }
+                            bot.logEmbed.setTitle("Rule case as Guilty");
+                            bot.logEmbed.addField("Perpetrator", msg.member.displayName)
+                                .addField("Reason", args.slice(0, args.length - 1));
+                            bot.logs.send(bot.logEmbed);
+                            bot.courtThing = "";
+                            m.delete();
+                        });
                 } else if (args.length === 0) {
                     guilty.setDescription(msg.member.displayName + ", please provide a reason.");
                     guilty.setFooter('No reason found.');
