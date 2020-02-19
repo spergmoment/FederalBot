8988115259b7739067e9171f52efdb75b405b94f
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-19 13:03:51 -0500

    brugga

--- src/commands/mistrial.js
+++ src/commands/mistrial.js
@@ -7,37 +7,40 @@ exports.run = (msg, bot, args) => {
             .setColor("RANDOM");
         if (msg.channel.parent.name === "court") {
             if (msg.member.user.id === bot.judgeToUse.user.id) {
-                mis.setDescription(msg.member.displayName + ", you have declared this case as a MISTRIAL.");
-                mis.setFooter('Case declared a mistrial');
-                bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Court"));
-                msg.channel.send(mis);
-                var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
-                var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
-                msg.channel.replacePermissionOverwrites({
-                    overwrites: [{
-                        id: cj.id,
-                        deny: ['SEND_MESSAGES'],
+                msg.channel.send("Marking this case as a mistrial...")
+                    .then(m => {
+                        mis.setDescription(msg.member.displayName + ", you have declared this case as a MISTRIAL.");
+                        mis.setFooter('Case declared a mistrial');
+                        bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Court"));
+                        msg.channel.send(mis);
+                        var cj = msg.guild.roles.find(r => r.name === "Chief Justice");
+                        var cp = msg.guild.roles.find(r => r.name === "Chief of Police");
+                        msg.channel.replacePermissionOverwrites({
+                            overwrites: [{
+                                id: cj.id,
+                                deny: ['SEND_MESSAGES'],
                     }, {
-                        id: cp.id,
-                        deny: ['SEND_MESSAGES'],
+                                id: cp.id,
+                                deny: ['SEND_MESSAGES'],
                     }, {
-                        id: bot.detainer.id,
-                        deny: ['SEND_MESSAGES'],
+                                id: bot.detainer.id,
+                                deny: ['SEND_MESSAGES'],
                     }, {
-                        id: bot.judgeToUse.user.id,
-                        deny: ['SEND_MESSAGES'],
+                                id: bot.judgeToUse.user.id,
+                                deny: ['SEND_MESSAGES'],
                     }, {
-                        id: bot.courtThing.user.id,
-                        deny: ['SEND_MESSAGES'],
+                                id: bot.courtThing.user.id,
+                                deny: ['SEND_MESSAGES'],
                     }, {
-                            id: msg.guild.defaultRole.id,
-                            deny: ["SEND_MESSAGES"],
+                                id: msg.guild.defaultRole.id,
+                                deny: ["SEND_MESSAGES"],
                         }, ],
-                });
-                bot.logEmbed.setTitle("Rule case as a Mistrial")
-                .addField("Perpetrator", msg.member.displayName);
-                bot.logs.send(bot.logEmbed);
-                bot.courtThing = "";
+                        });
+                        bot.logEmbed.setTitle("Rule case as a Mistrial")
+                            .addField("Perpetrator", msg.member.displayName);
+                        bot.logs.send(bot.logEmbed);
+                        bot.courtThing = "";
+                    });
             }
         } else {
             mis.setDescription(msg.member.displayName + ", this command may only be used in a court case.");
