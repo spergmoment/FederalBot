d449abfeac6e0e2fe5bac34bcb870dcf6a7939f6
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-24 14:11:53 -0500

    efficiency, readability

--- src/commands/resign.js
+++ src/commands/resign.js
@@ -49,8 +49,10 @@ exports.run = (msg, bot, args) => {
             .then(m => {
                 msg.member.removeRole(msg.guild.roles.find(r => r.name === pos))
                     .catch(console.error);
-                resign.setDescription(msg.member.displayName + ", you have successfully resigned from the position of " + pos + ".");
-                resign.setFooter('Resigned from ' + pos);
+                resign.setDescription(msg.member.displayName + 
+                                      ", you have successfully resigned from the position of " + 
+                                      pos + ".")
+                .setFooter('Resigned from ' + pos);
                 bot.logEmbed.setTitle("Action: Resign")
                     .addField("Perpetrator", msg.member.tag)
                     .addField("Position", pos);
@@ -59,8 +61,9 @@ exports.run = (msg, bot, args) => {
                 msg.channel.send(resign);
             });
     } else {
-        resign.setDescription("You are currently unable to resign. If you are in a position which you believe you should be able to resign, please contact Sperg.");
-        resign.setFooter('You lack any governmental position.');
+        resign.setDescription("You are currently unable to resign. If you are in a position " +
+                              "which you believe you should be able to resign, please contact Sperg.")
+        .setFooter('You lack any governmental position.');
         msg.channel.send(resign);
     }
 };
