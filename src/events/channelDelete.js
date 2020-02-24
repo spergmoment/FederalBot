07bb521ee41bd254b70d90b4e8be3f63c152e002
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-24 12:32:05 -0500

    add time deleted

--- src/events/channelDelete.js
+++ src/events/channelDelete.js
@@ -1,4 +1,49 @@
 module.exports = (bot, ch) => {
+    function dateConvert() {
+        var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+        var weeks = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+  ];
+        var time = ch.createdAt;
+        var yr = time.getFullYear();
+        var mon = time.getMonth();
+        mon = months[mon];
+        var day = time.getDate(); // gets the day, e.g 27
+        var hr = time.getHours(); // gets the current hour, e.g 9, it is in 24 hour time and formatting it to 12 hour time has bad side effects, like making anything ≥ 10 be 0
+        var min = time.getMinutes(); // gets the current minutes, e.g. 12
+        var sec = time.getSeconds(); // gets current seconds, e.g. 34
+        var week = time.getDay(); // gets day of week, e.g. monday
+        week = weeks[week];
+        if (hr < 10) {
+            hr = "0" + hr.charAt(0);
+        }
+        if (min < 10) {
+            min = "0" + min.charAt(0);
+        }
+        if (sec < 10) {
+            sec = "0" + sec.charAt(0);
+        }
+        return week + ", " + mon + " " + day + ", " + yr + ", at " + hr + ":" + min + ":" + sec;
+    }
     if(ch.type==="dm") return;
     const logs = ch.guild.channels.find(r => r.name === ("logs"));
     const Discord = require("discord.js");
@@ -11,6 +56,7 @@ module.exports = (bot, ch) => {
             logsEmbed.setTitle("Action: Delete Channel")
                 .addField("Channel Name", ch.name)
                 .addField("Perpetrator", e.executor.username)
+                .addField("Time Created", dateConvert())
                 .addField("IDs", "```Channel ID: " + ch.id + "```");
             logs.send(logsEmbed);
         }
