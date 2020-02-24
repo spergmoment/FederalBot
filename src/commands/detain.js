2f02414782eb71d1d801b5d61d88943d0bf9965b
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-24 14:04:09 -0500

    efficiency

--- src/commands/detain.js
+++ src/commands/detain.js
@@ -1,7 +1,7 @@
 exports.run = async (msg, bot, args) => {
     const Discord = require("discord.js");
     let member = msg.mentions.members.first();
-    bot.evidence=args.slice(2);
+    bot.evidence = args.slice(2);
     if (!msg.member.roles.find(r => r.name === "Officer") && !msg.member.roles.find(r => r.name === "Chief of Police")) {
         return msg.channel.send("You can not use this command!");
     }
@@ -26,7 +26,7 @@ exports.run = async (msg, bot, args) => {
             });
     }
     if (!args[1]) {
-        if(!member) return;
+        if (!member) return;
         msg.channel.send("Which law did they break?");
         await msg.channel.awaitMessages(m => m.author.id === msg.author.id, {
                 max: 1,
@@ -47,10 +47,10 @@ exports.run = async (msg, bot, args) => {
     } else {
         bot.reason = args[1];
     }
-    if(args.length<3) {
-        if(!bot.reason) return;
-       msg.channel.send("Please provide evidence.");
-    await msg.channel.awaitMessages(m => m.author.id === msg.author.id, {
+    if (args.length < 3) {
+        if (!bot.reason) return;
+        msg.channel.send("Please provide evidence.");
+        await msg.channel.awaitMessages(m => m.author.id === msg.author.id, {
                 max: 1,
                 time: 30000,
                 errors: ['time']
@@ -68,38 +68,37 @@ exports.run = async (msg, bot, args) => {
                 return msg.channel.send("Time limit reached, try again");
             });
     }
-    if(!member||!bot.reason||!bot.evidence) return;
-    msg.channel.send("Detaining " + member.displayName + "...").then(m => {
-    const det = new Discord.RichEmbed()
-        .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-        .setTimestamp()
-        .setColor('RANDOM');
-    bot.detainer = msg.member;
-    let role = msg.guild.roles.find(r => r.name === "Detained");
-    det.setDescription(`${msg.member.displayName}, I have detained ${member.displayName}` + 
-                       `, for reason ${args[1]}, with evidence ${bot.evidence}. A judge must **;approve** this detain within **5 minutes** or you will be IMPEACHED!`);
-    /*the above was split in 2 to take up less space*/
-    det.setFooter(`User ${member.displayName} has been detained.`);
-    bot.logEmbed.setTitle("Detain")
-    .addField("User", member.tag)
-    .addField("Perpetrator", msg.member.tag)
-    .addField("Reason", bot.reason)
-    .addField("Evidence", bot.evidence);
-    bot.logs.send(bot.logEmbed);
-    member.addRole(role) // the good stuff starts now..
-        .catch(console.error);
-    setTimeout(
-        () => {
-            if (!member.roles.find(r => r.name === "Court")) { // checks if the detainment was approved by a judge or not
-                msg.member.removeRole(msg.guild.roles.find(r => r.name === "Officer"))
-                    .catch(console.error);
-                member.removeRole(role)
-                    .catch(console.error);
-                det.setDescription(msg.member.displayName + ", you have been impeached because no judges approved this detainment in the 5 minutes.");
-                det.setFooter('Impeached from Officer.');
-            }
-        }, 300000); // waits 5 minutes to check whether they have been approved, does nothing if it has but impeaches you if it hasn't
-    msg.channel.send(det);
-    m.delete();
-});
+    if (!member || !bot.reason || !bot.evidence) return;
+    msg.channel.send("Detaining " + member.displayName + "...")
+        .then(m => {
+            const det = new Discord.RichEmbed()
+                .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
+                .setTimestamp()
+                .setColor('RANDOM');
+            bot.detainer = msg.member;
+            let role = msg.guild.roles.find(r => r.name === "Detained");
+            det.setDescription(`${msg.member.displayName}, I have detained ${member.displayName}` + `, for reason ${args[1]}, with evidence ${bot.evidence}. ` + `A judge must **;approve** this detain within **5 minutes** or you will be IMPEACHED!`)
+                .setFooter(`User ${member.displayName} has been detained.`);
+            bot.logEmbed.setTitle("Detain")
+                .addField("User", member.tag)
+                .addField("Perpetrator", msg.member.tag)
+                .addField("Reason", bot.reason)
+                .addField("Evidence", bot.evidence);
+            bot.logs.send(bot.logEmbed);
+            member.addRole(role) // the good stuff starts now..
+                .catch(console.error);
+            setTimeout(
+                () => {
+                    if (!member.roles.find(r => r.name === "Court")) { // checks if the detainment was approved by a judge or not
+                        msg.member.removeRole(msg.guild.roles.find(r => r.name === "Officer"))
+                            .catch(console.error);
+                        member.removeRole(role)
+                            .catch(console.error);
+                        det.setDescription(msg.member.displayName + ", you have been impeached because no judges approved this detainment in the 5 minutes.")
+                            .setFooter('Impeached from Officer.');
+                    }
+                }, 300000); // waits 5 minutes to check whether they have been approved, does nothing if it has but impeaches you if it hasn't
+            msg.channel.send(det);
+            m.delete();
+        });
 };
