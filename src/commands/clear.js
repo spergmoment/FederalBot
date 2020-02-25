a16fcb3b16a2834c1872516cb402a69f06bda82c
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-25 10:47:26 -0500

    add bot owner perms, efficiency

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,51 +1,40 @@
 exports.run = (msg, bot, args) => {
-    const Discord = require("discord.js");
-    if (msg.member.roles.find(r => r.name === "Congress")) {
-        msg.delete();
-        msg.channel.send("Clearing...")
-            .then(async m => {
-                try {
-                    if (args[0]) {
-                        await msg.channel.fetchMessages({
-                                limit: args[0],
-                            })
-                            .then(async msgs => {
-                                await msg.channel.bulkDelete(msgs)
-                                    .catch(error => console.log(error.stack));
-                            });
-                        await m.edit("Cleared " + args[0] + " messages.")
-                            .then(async m => {
-                                setTimeout(async () => {
-                                    await m.delete();
-                                }, 2500);
-                            });
-                    } else {
-                        await msg.channel.fetchMessages({
-                                limit: 20,
-                            })
-                            .then(async msgs => {
-                                await msg.channel.bulkDelete(msgs)
-                                    .catch(error => console.log(error.stack));
-                            });
-                        await m.edit("Cleared 20 messages.")
-                            .then(async m => {
-                                setTimeout(async () => {
-                                    await m.delete();
-                                }, 2500);
-                            });
-                    }
-                } catch (err) {
-                    msg.channel.send("An error occurred:\n" + err);
+    if (!msg.member.roles.find(r => r.name === "Congress") && !msg.member.roles.find(r => r.name === "Bot Owner")) return msg.channel.send("You must be in Congress to use this command.");
+    msg.delete();
+    msg.channel.send("Clearing...")
+        .then(async m => {
+            try {
+                if (args[0]) {
+                    await msg.channel.fetchMessages({
+                            limit: args[0],
+                        })
+                        .then(async msgs => {
+                            await msg.channel.bulkDelete(msgs)
+                                .catch(error => console.log(error.stack));
+                        });
+                    await m.edit("Cleared " + args[0] + " messages.")
+                        .then(async m => {
+                            setTimeout(async () => {
+                                await m.delete();
+                            }, 2500);
+                        });
+                } else {
+                    await msg.channel.fetchMessages({
+                            limit: 20,
+                        })
+                        .then(async msgs => {
+                            await msg.channel.bulkDelete(msgs)
+                                .catch(error => console.log(error.stack));
+                        });
+                    await m.edit("Cleared 20 messages.")
+                        .then(async m => {
+                            setTimeout(async () => {
+                                await m.delete();
+                            }, 2500);
+                        });
                 }
-            });
-    } else {
-        const bruh = new Discord.RichEmbed()
-            .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
-            .setTimestamp()
-            .setColor('#' + (0x1000000 + (Math.random()) * 0xffffff)
-                .toString(16)
-                .substr(1, 6))
-            .setDescription("You lack permissions to use this command.");
-        msg.channel.send(bruh);
-    }
+            } catch (err) {
+                msg.channel.send("An error occurred:\n" + err);
+            }
+        });
 };
