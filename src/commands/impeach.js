cf7bb45e1382c4ed05ed4d296c685e060229af47
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-25 10:54:01 -0500

    fixes, efficiency

--- src/commands/impeach.js
+++ src/commands/impeach.js
@@ -21,55 +21,42 @@ exports.run = async (msg, bot, args) => {
                 return msg.channel.send("Time limit reached, try again.");
             });
     }
-    let send = ("You either do not have the correct role for you to impeach " + member.user.username + ", or another error has occured. Please try again later."); // saves some space
     const peach = new Discord.RichEmbed()
         .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
         .setTimestamp()
         .setColor('RANDOM');
-    if (args) {
-        let role;
-        if (msg.member.roles.find(r => r.name === "Speaker of the House")) {
-            role = msg.guild.roles.find(r => r.name === "Congress");
-        }
-        if (msg.member.roles.find(r => r.name === "Chief Justice")) {
-            role = msg.guild.roles.find(r => r.name === "Judge");
-        }
-        if (msg.member.roles.find(r => r.name === "Chief of Police")) {
-            role = msg.guild.roles.find(r => r.name === "Officer");
-        }
-        if (role) {
-            if (member.roles.find(r => r.name === role.name)) {
-                msg.channel.send("Impeaching " + member.displayName + "...")
-                    .then(async m => {
-                        member.removeRole(role)
-                            .catch(console.error); // lol
-                        if (role.name === "Congress") {
-                            if (member.roles.find(r => r.name === "House")) {
-                                member.removeRole(msg.guild.roles.find(r => r.name === "House"));
-                            }
-                            if (member.roles.find(r => r.name === "Senate")) {
-                                member.removeRole(msg.guild.roles.find(r => r.name === "Senate"));
-                            }
-                        }
-                        peach.setDescription(msg.member.displayName + ", I have impeached " + member.displayName + " from " + role.name + ".");
-                        peach.setFooter('Impeached ' + member.displayName + ' from ' + role.name + ".");
-                        bot.logEmbed.setTitle("Impeach")
-                            .addField("User", member.tag)
-                            .addField("Perpetrator", msg.member.tag)
-                            .addField("Position", role.name);
-                        bot.logs.send(bot.logEmbed);
-                        m.delete();
-                    });
-            } else {
-                peach.setDescription(send);
-            }
-        } else {
-            peach.setDescription("You lack the proper role to use this command.")
-                .setFooter("You lack a branch role in the Permissions Object.");
-        }
-    } else {
-        peach.setDescription("Args not found. Please resend the command with proper arguments.")
-            .setFooter('Couldn\'t find args.');
+    let role;
+    if (msg.member.roles.find(r => r.name === "Speaker of the House")) {
+        role = msg.guild.roles.find(r => r.name === "Congress");
     }
-    msg.channel.send(peach);
+    if (msg.member.roles.find(r => r.name === "Chief Justice")) {
+        role = msg.guild.roles.find(r => r.name === "Judge");
+    }
+    if (msg.member.roles.find(r => r.name === "Chief of Police")) {
+        role = msg.guild.roles.find(r => r.name === "Officer");
+    }
+    if (!role) return msg.channel.send("You lack permission to use this role.");
+    if (!member.roles.find(r => r.name === role.name)) return msg.channel.send("You either do not have the correct role for you to impeach " + 
+                member.user.username + ", or another error has occured. Please try again later.");
+    msg.channel.send("Impeaching " + member.displayName + "...")
+        .then(async m => {
+            member.removeRole(role)
+                .catch(console.error); // lol
+            if (role.name === "Congress") {
+                if (member.roles.find(r => r.name === "House")) {
+                    member.removeRole(msg.guild.roles.find(r => r.name === "House"));
+                }
+                if (member.roles.find(r => r.name === "Senate")) {
+                    member.removeRole(msg.guild.roles.find(r => r.name === "Senate"));
+                }
+            }
+            peach.setDescription(msg.member.displayName + ", I have impeached " + member.displayName + " from " + role.name + ".");
+            peach.setFooter('Impeached ' + member.displayName + ' from ' + role.name + ".");
+            bot.logEmbed.setTitle("Impeach")
+                .addField("User", member.tag)
+                .addField("Perpetrator", msg.member.tag)
+                .addField("Position", role.name);
+            bot.logs.send(bot.logEmbed);
+            m.delete();
+        });
 };
