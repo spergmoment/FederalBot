c49a15cf3acafca2238d70ab74b355c747ed48d4
sperg <58081117+spergmoment@users.noreply.github.com>
2020-03-03 10:16:04 -0500

    sTrIng literals

--- src/commands/mistrial.js
+++ src/commands/mistrial.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'mistrial',
     desc: 'For the judge of a case to rule the case as a `mistrial`.',
     usage: ';mistrial',
-    extraNotes: 'A mistrial is for an accidental, or otherwise unlawful, " + "incorrect, or overall wrong detainment.',
+    extraNotes: "A mistrial is for an accidental, or otherwise unlawful, " + 
+                "incorrect, or overall wrong detainment.",
     execute(msg, bot, args) {
         if (bot.judgeToUse && bot.detainer && bot.courtThing) {
             const Discord = require("discord.js");
@@ -10,11 +11,13 @@ module.exports = {
                 .setAuthor(msg.author.tag, msg.author.avatarURL, msg.author.avatarURL)
                 .setTimestamp()
                 .setColor("RANDOM");
-            if (msg.channel.parent.name !== "court") return msg.channel.send("Please use this in a valid court case.");
-            if (msg.member.user.id !== bot.judgeToUse.user.id) return msg.channel.send("You must be the judge of a case to use this command.");
+            if (msg.channel.parent.name !== "court") 
+                return msg.channel.send("Please use this in a valid court case.");
+            if (msg.member.user.id !== bot.judgeToUse.user.id) 
+                return msg.channel.send("You must be the judge of a case to use this command.");
             msg.channel.send("Marking this case as a mistrial...")
                 .then(m => {
-                    mis.setDescription(msg.member.displayName + ", you have declared this case as a MISTRIAL.")
+                    mis.setDescription(`${msg.member.displayName}, you have declared this case as a MISTRIAL.`)
                         .setFooter('Case declared a mistrial');
                     bot.courtThing.removeRole(msg.guild.roles.find(r => r.name === "Court"));
                     msg.channel.send(mis);
