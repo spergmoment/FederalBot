d1ec5dabf15755f9b79e73aca4215259f04f3453
sperg <58081117+spergmoment@users.noreply.github.com>
2020-03-03 10:15:04 -0500

    sTRING LIterals

--- src/commands/law.js
+++ src/commands/law.js
@@ -4,8 +4,9 @@ module.exports = {
     usage: ';law (law)',
     examples: ';law 2 // returns law 2, raiding.',
     execute(msg, bot, args) {
-        if (args[0] > bot.laws.length || args[0] < 1) return msg.channel.send("That is not a valid law!");
-        msg.channel.send("Fetching law " + args[0] + "...")
+        if (args[0] > bot.laws.length || args[0] < 1) 
+            return msg.channel.send("That is not a valid law!");
+        msg.channel.send(`Fetching law ${args[0]}...`)
             .then(m => {
                 const Discord = require("discord.js");
                 var lawThing = (bot.laws[args[0] - 1]);
@@ -13,7 +14,7 @@ module.exports = {
                     .setTimestamp()
                     .setColor('RANDOM')
                     .setDescription(lawThing)
-                    .setFooter('Law ' + args[0]);
+                    .setFooter(`Law ${args[0]}`);
                 msg.channel.send(lawSend);
                 m.delete();
             });
