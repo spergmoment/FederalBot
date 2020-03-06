15206f98b59a4836f74c1051b2f1b258055bc575
sperg <58081117+spergmoment@users.noreply.github.com>
2020-03-06 10:15:26 -0500

    forgot to change the thing

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -8,8 +8,8 @@ module.exports = (bot, mem) => {
         logsEmbed.setTitle("User Left")
             .addField("User", mem.user.tag)
             .addField("Avatar URL", mem.user.avatarURL)
-            .addField("Account Creation Date", bot.dateConvert(mem.user.createdAt))
-            .addField("Time Left", bot.dateConvert(Date()))
+            .addField("Account Creation Date", bot.format(mem.user.createdAt))
+            .addField("Time Left", bot.format(Date()))
             .addField("IDs", `\`\`\`User: ${mem.user.id}\`\`\``);
         logs.send(logsEmbed);
     }
